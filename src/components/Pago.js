a9c306ed600071c10eca9845c479d4ebf2d7e85e
abigahil.delgado <abigahil.delgado@ine.mx>
2022-10-04 19:33:10 -0500

    feat: formato de fecha y eliminar pago.

--- src/components/Pago.js
+++ src/components/Pago.js
@@ -1,5 +1,8 @@
 import { Component, alert } from "react";
 import Button from "./Button";
+import moment from "moment/moment";
+import 'moment/locale/es';
+import axios from 'axios';
 import Modal from "./Modal";
 
 
@@ -38,8 +41,22 @@ class Pago extends Component {
         
     }
 
-
-  
+    eliminar = () => {
+        const { pago } = this.props
+        const id = pago.objectId
+        console.log(id)
+        axios.delete(`/cliente/pago/${ id }`)
+        .then(res => {
+            console.log(res);
+            console.log(res.data);
+            if(res.status === 200){
+                console.log('El pago fue eliminado exitosamente')
+            } else {
+                console.log(res.message)
+            }
+        })
+        
+    }
 
     render() {
         const { pago } = this.props
@@ -50,11 +67,13 @@ class Pago extends Component {
                     <li style={styles.pag}>
                         <p>Paquete contratado: <b>{pago.paqueteTipo}</b></p>
                         <p>Pago $: <b>{pago.pago} </b></p>
-                        <p>Fecha: <b>{pago.fecha}</b></p>
-                         <Button onClick={() => this.mostrarModal()}>
-                            Eliminar pago          
+                        <p>Fecha: <b>{ moment(pago.fecha).format("dddd DD [de] MMMM - HH:mm a")}</b></p>                 
+                        <Button onClick={() => this.mostrarModal()}>
+                            Imprimir
                         </Button>
-                       
+                        <Button onClick={()=> {this.eliminar(); this.mostrarModal()}}>
+                            Eliminar pago
+                        </Button>
                     </li>
                 </ul>
             </div>
